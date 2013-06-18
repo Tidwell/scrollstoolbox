8b27892d142d4b388fa832094a6a64e4cf0b57bd
Aaron Tidwell <aaron.tidwell@gmail.com>
2013-06-18 19:39:29 -0400

    first

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('scrollstoolboxApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
