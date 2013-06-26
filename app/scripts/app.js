394dff2883f010c689ba1b2dc1c743e77ab3f65e
Aaron Tidwell <aaron.tidwell@gmail.com>
2013-06-26 02:16:05 -0400

    fixing shit so it doesnt error with the tutorial, tutorial borked

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('scrollstoolboxApp', ['ui.bootstrap'])
+angular.module('scrollstoolboxApp', [])
 	.config(function($routeProvider, $locationProvider) {
 	$locationProvider.html5Mode(true);
 	$routeProvider
