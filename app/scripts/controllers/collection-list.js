3280f1402aa7b7cfe2b04f41f9949d710bf84f62
Aaron Tidwell <aaron.tidwell@gmail.com>
2013-07-15 20:48:24 -0400

    working collection view

--- app/scripts/controllers/collection-list.js
+++ app/scripts/controllers/collection-list.js
@@ -4,4 +4,10 @@ angular.module('scrollstoolboxApp')
 	.controller('CollectionListCtrl', function($scope, collectionList, $routeParams) {
 		$scope.username = $routeParams.username;
 		$scope.collection = collectionList.get($scope.username);
+
+		$scope.generateImagePath = function(name) {
+			if (name) {
+				return '/img/scrolls-cards/'+name.toLowerCase().replace(/ /g,'').replace(/'/g,'')+'.png';
+			}
+		};
 	});
